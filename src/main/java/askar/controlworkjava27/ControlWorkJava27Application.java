fab0e3f8f76d7b2b24e50737362bc792f0c5491d
Askar <nurbekovaskar099@gmail.com>
2026-05-02 11:21:58 +0600

    Starting pack

---
+++ src/main/java/askar/controlworkjava27/ControlWorkJava27Application.java
@@ -0,0 +1,13 @@
+package askar.controlworkjava27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlWorkJava27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlWorkJava27Application.class, args);
+    }
+
+}
---
+++ src/test/java/askar/controlworkjava27/ControlWorkJava27ApplicationTests.java
@@ -0,0 +1,13 @@
+package askar.controlworkjava27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlWorkJava27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
